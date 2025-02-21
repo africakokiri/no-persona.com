432431918aea67f450838796792e5e65bc7bf5eb
Choi Chan Hwan <africakokiri@gmail.com>
2025-02-22 02:15:41 +0900

    Description 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,21 @@ import Image from "next/image";
 
 export default function page() {
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <div className="flex flex-col items-center justify-center">
+    <div className="flex h-full w-full items-center justify-center gap-8">
+      <div className="flex flex-col items-center justify-center gap-8">
         <Image
           src="/icons/kokiri.svg"
           alt=""
           width={128}
           height={128}
         />
-        <p>Kokiri.social</p>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <p className="text-3xl font-light">kokiri.social</p>
+          <p className="font-light">
+            Just tell me whatever&apos;s on your mind.
+          </p>
+        </div>
       </div>
-      <form action=""></form>
     </div>
   );
 }
