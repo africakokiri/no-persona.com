5fa918a53fb4fae37415f3ed82d7199664df3967
Choi Chan Hwan <africakokiri@gmail.com>
2025-02-22 03:56:44 +0900

    UserInput 컴포넌트 기본적인 로직 구성

--- src/components/user-input.tsx
+++ src/components/user-input.tsx
@@ -1,30 +1,42 @@
 "use client";
 
 import { Inter } from "next/font/google";
-import { type FormEvent, useState } from "react";
+import { type FormEvent, useRef, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const UserInput = () => {
   const [userInput, setUserInput] = useState("");
+  const [password, setPassword] = useState("");
+  const textareaRef = useRef(null);
+  const inputRef = useRef(null);
 
   const handleSubmit = (e: FormEvent) => {
     // Onsubmit refresh 방지
     e.preventDefault();
+
+    // user input의 길이가 5 미만이면 DB 접근 불가
+    if (userInput.length < 5 && textareaRef.current) {
+      console.log(textareaRef.current);
+    }
+
+    // password의 길이가 6 미만이면 DB 접근 불가
+    if (password.length < 6 && inputRef.current) {
+      console.log(inputRef.current);
+    }
   };
 
   return (
     <form
-      onSubmit={(e) =>
-        userInput.length > 0 ? handleSubmit(e) : e.preventDefault()
-      }
+      onSubmit={(e) => handleSubmit(e)}
       className={`${inter.className} flex w-full flex-col items-end gap-4
 px-4 md:w-[800px] md:gap-8`}
     >
       <TextareaAutosize
         maxRows={10}
         autoFocus
+        ref={textareaRef}
         value={userInput}
         onChange={(e) => setUserInput(e.target.value)}
         maxLength={1000}
@@ -50,6 +62,9 @@ py-1"
           type="password"
           minLength={6}
           maxLength={20}
+          ref={inputRef}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="Enter password here."
           className="w-full rounded-none border-[1px] border-black px-3
 py-1 outline-none md:block md:w-[200px]"
--- src/server/database.ts
+++ src/server/database.ts
@@ -2,7 +2,12 @@
 
 import { createClient } from "@/server/supabase";
 
-export const connectDB = async () => {
+export const connectSupabase = async (
+  userInput: string,
+  password: string
+) => {
+  console.log(userInput, password);
+
   const supabase = await createClient();
   const { data: instruments } = await supabase
     .from("instruments")
