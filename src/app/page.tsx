74fb8a0a6cc5dc747fb06dddf1ea272eac1dd93a
Choi Chan Hwan <africakokiri@gmail.com>
2025-02-22 01:56:26 +0900

    Logo 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
 export default function page() {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      Hello World
+      <div className="flex flex-col items-center justify-center">
+        <Image
+          src="/icons/kokiri.svg"
+          alt=""
+          width={128}
+          height={128}
+        />
+        <p>Kokiri.social</p>
+      </div>
+      <form action=""></form>
     </div>
   );
 }
