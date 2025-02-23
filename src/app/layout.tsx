25474e715a464dd4ec9c2ee5e9a65aef5317f962
Choi Chan Hwan <africakokiri@gmail.com>
2025-02-24 01:18:11 +0900

    Metadata 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,11 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: ""
+  title: "Kokiri",
+  description: "Write down everything you wanna say.",
+  icons: {
+    icon: "/icons/kokiri.svg"
+  }
 };
 
 export default function RootLayout({
