43df8a061c2f1f1e7e81af9e789545c6ccc4d00a
Choi Chan Hwan <africakokiri@gmail.com>
2025-02-24 01:28:03 +0900

    페이지 title, description 스타일링

--- src/components/page-title.tsx
+++ src/components/page-title.tsx
@@ -2,15 +2,19 @@ import Image from "next/image";
 
 export const PageTitle = () => {
   return (
-    <header className="flex flex-col items-center justify-center">
+    <header className="flex flex-col items-center justify-center gap-8">
       <Image
         src="/icons/kokiri.svg"
         alt=""
         width={128}
         height={128}
       />
-      <h1>kokiri.social</h1>
-      <p>Tell me anything you wanna say!</p>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-3xl font-light">kokiri.social</h1>
+        <p className="text-lg font-light">
+          Tell me anything you wanna say!
+        </p>
+      </div>
     </header>
   );
 };
