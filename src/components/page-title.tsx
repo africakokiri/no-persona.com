d43e61bb2574eeb820da4f110d12e45f8d533ceb
Choi Chan Hwan <africakokiri@gmail.com>
2025-02-24 01:31:06 +0900

    DarkmodeSwitch 컴포넌트 적용

--- src/components/page-title.tsx
+++ src/components/page-title.tsx
@@ -1,14 +1,19 @@
+import { DarkmodeSwitch } from "@/app/darkmode-switch";
+
 import Image from "next/image";
 
 export const PageTitle = () => {
   return (
     <header className="flex flex-col items-center justify-center gap-8">
-      <Image
-        src="/icons/kokiri.svg"
-        alt=""
-        width={128}
-        height={128}
-      />
+      <div className="flex flex-col items-center justify-center gap-4">
+        <Image
+          src="/icons/kokiri.svg"
+          alt=""
+          width={128}
+          height={128}
+        />
+      </div>
+      <DarkmodeSwitch />
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-3xl font-light">kokiri.social</h1>
         <p className="text-lg font-light">
