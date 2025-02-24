9753995ac7072634687de489c96a98d5ab746db8
Choi Chan Hwan <africakokiri@gmail.com>
2025-02-24 11:28:48 +0900

    SubmitModalBtn 모달 스타일링 및 UUID 적용

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -11,6 +11,21 @@ import {
 } from "@/components/ui/alert-dialog";
 
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const UuidAlert = () => {
+  return (
+    <div
+      className="flex items-center justify-between rounded-md border-[1px]
+border-black/20 p-2"
+    >
+      <p className="text-black">{uuidv4()}</p>
+      <button className="rounded-md bg-black px-2 py-1 text-white">
+        copy
+      </button>
+    </div>
+  );
+};
 
 export const SubmitModalBtn = ({ userInput }: { userInput: string }) => {
   const [openModal, setOpenModal] = useState(false);
@@ -38,8 +53,19 @@ export const SubmitModalBtn = ({ userInput }: { userInput: string }) => {
           <AlertDialogHeader>
             <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
             <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete
-              your account and remove your data from our servers.
+              <div className="space-y-2">
+                <UuidAlert />
+                <p className="leading-7">
+                  This action cannot be undone. This will permanently post
+                  your comment.
+                  <br />
+                  <span
+                    className="text-red-500 underline underline-offset-4"
+                  >
+                    To delete the comment, you need the code above.
+                  </span>
+                </p>
+              </div>
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
