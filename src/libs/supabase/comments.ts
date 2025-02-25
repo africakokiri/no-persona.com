96db5c026af8013ec364854fe652a33a2a9710e0
Choi Chan Hwan <africakokiri@gmail.com>
2025-02-25 11:32:24 +0900

    Supabase server side client 연결 함수 추가

---
+++ src/libs/supabase/comments.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { createClient } from "@/libs/supabase/server";
+
+const supabase = await createClient();
+
+export const comments = async (data: {
+  userInput: string;
+  password: string;
+}) => {
+  const { data: newComment, error } = await supabase
+    .from("comments")
+    .insert(data)
+    .select();
+
+  if (error) throw error;
+
+  return newComment;
+};
+
+export const getComments = async () => {
+  const { data, error } = await supabase.from("comments").select();
+
+  if (error) throw error;
+
+  return data;
+};
