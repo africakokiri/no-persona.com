233540d4b13b7e6896932a3c56e82686b1721021
Choi Chan Hwan <africakokiri@gmail.com>
2025-02-27 08:10:09 +0900

    Comments mock data 분리

---
+++ src/mock/comments.ts
@@ -0,0 +1,45 @@
+export const comments = [
+  {
+    id: 13,
+    comment: "1234",
+    password: "1234",
+    created_at: "2025-02-25T04:04:08.143245+00:00"
+  },
+  {
+    id: 22,
+    comment: "1234",
+    password: "1234",
+    created_at: "2025-02-25T23:07:31.330323+00:00"
+  },
+  {
+    id: 23,
+    comment: "test",
+    password: "test",
+    created_at: "2025-02-25T23:08:21.723272+00:00"
+  },
+  {
+    id: 29,
+    comment: "Test comment",
+    password: "1234",
+    created_at: "2025-02-25T23:19:51.084659+00:00"
+  },
+  {
+    id: 40,
+    comment: "3113",
+    password: "3113",
+    created_at: "2025-02-25T23:51:48.027767+00:00"
+  },
+  {
+    id: 41,
+    comment: "zxcv",
+    password: "zxcv",
+    created_at: "2025-02-25T23:52:30.871974+00:00"
+  },
+  {
+    id: 50,
+    comment:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi commodi doloribus eligendi non accusantium minima ipsa sit. Dolor saepe sunt, quibusdam ducimus voluptate suscipit commodi cum praesentium beatae molestias aliquid.",
+    password: "1234",
+    created_at: "2025-02-27T23:52:30.871974+00:00"
+  }
+];
