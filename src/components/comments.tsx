8bda424c45ee58a73e0067a72f47b755c609a2a4
Choi Chan Hwan <africakokiri@gmail.com>
2025-02-28 10:24:18 +0900

    Delete 버튼 Modal 적용
    기능구현 X

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -1,5 +1,16 @@
 "use client";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from "@/components/ui/alert-dialog";
 import { getComments } from "@/libs/supabase/handle-comments";
 import { useNewCommentStore } from "@/libs/zustand/store";
 
@@ -28,39 +39,66 @@ export const Comments = () => {
   }, [newComments]);
 
   return (
-    <ul className="space-y-8">
-      {comments
-        .sort((a, b) => {
-          if (a.created_at > b.created_at) return -1;
-          if (a.created_at < b.created_at) return 1;
+    <>
+      <ul className="space-y-8">
+        {comments
+          .sort((a, b) => {
+            if (a.created_at > b.created_at) return -1;
+            if (a.created_at < b.created_at) return 1;
 
-          return 0;
-        })
-        .map(({ comment, created_at }) => {
-          const date = new Date(created_at);
+            return 0;
+          })
+          .map(({ comment, created_at }) => {
+            const date = new Date(created_at);
 
-          const formattedTime = date.toLocaleString(undefined, {
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit"
-          });
+            const formattedTime = date.toLocaleString(undefined, {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit"
+            });
 
-          return (
-            <li
-              key={created_at}
-              className="w-full space-y-2 rounded-md border-[1px]
+            return (
+              <li
+                key={created_at}
+                className="w-full space-y-4 rounded-md border-[1px]
 border-black bg-white p-4"
-            >
-              <p>{comment}</p>
-              <p className="text-right text-sm font-light">
-                {formattedTime}
-              </p>
-            </li>
-          );
-        })}
-    </ul>
+              >
+                <p>{comment}</p>
+                <p className="flex justify-between text-right text-sm">
+                  <AlertDialog>
+                    <AlertDialogTrigger className="text-red-500">
+                      Delete
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader className="space-y-4">
+                        <AlertDialogTitle>
+                          Are you absolutely sure?
+                        </AlertDialogTitle>
+                        <AlertDialogDescription asChild>
+                          <input
+                            type="password"
+                            placeholder="Enter the password"
+                            maxLength={16}
+                            className="w-full rounded-md border-[1px]
+border-black px-3 py-2 text-black"
+                          />
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction>Continue</AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                  <span className="font-light">{formattedTime}</span>
+                </p>
+              </li>
+            );
+          })}
+      </ul>
+    </>
   );
 };
